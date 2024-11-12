e269c0f16a8b99ea1e56e140cf4dcb01303bb309
Dmitry Prigodich <dmitrijs.prigodics@gmail.com>
2024-11-13 00:24:59 +0200

    Rewritten function of data gathering

--- constants.py
+++ constants.py
@@ -2,10 +2,11 @@ URL_HWM_DB = 'https://hwm.puzzak.page/'
 OUTPUT = 'OUTPUT.md'
 
 LOCATOR_ENTRYHEAD = "//div[@class='entryhead']/b"
-# '.entryhead b'
+LOCATOR_ENTRIES = "//*[@class='entry']"
 LOCATOR_ENTRYITEM = "//div[@class='entryitem'][b[@class='entryitemname' and contains(text(), '{}')]]/a"
-# ".entryitem:has(b.entryitemname:has-text('{}')) a.entryitemvalue"
 
 #modules
 LOCATOR_EVENTDATA = "#EventData-module"
+LOCATOR_QUESTDATA = "#QuestData-module"
 LOCATOR_QUESTLINEDATA = "#QuestLineData-module"
+LOCATOR_STRINGDATA = "#StringData-module"
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,12 +1,17 @@
 import constants
-import pytest
 import utils
 
-def test_get_events_list(page):
+def test_get_data(page):
     check_page_loaded(page)
-    get_events(page)
-    wait_a_bit(page)
 
+    dict_stringdata = get_entry_items(page, constants.LOCATOR_STRINGDATA)
+
+    for entryhead, entryitems in dict_stringdata.items():
+        utils.write_to_file(f"* {entryhead}\n")
+        for name, value in entryitems.items():
+            utils.write_to_file(f"  * {name}: {value}\n")
+
+    wait_a_bit(page)
 
 def check_page_loaded(page):
     # page.goto(constants.URL_HWM_DB)
@@ -39,46 +44,47 @@ def get_events(page):
 
         list_entries_grouped[group_name].append(event_name)
 
+    utils.write_to_file("h1. EVENTS:\n")
     for group, names in list_entries_grouped.items():
-        utils.write_to_file("h1. EVENTS:\n\n")
-        utils.write_to_file("* {}".format(group))
-        for name in names:
-            print(f"  - {name}")
-            utils.write_to_file("  * {}".format(name))
+        utils.write_to_file("\n* {}\n".format(group))
+        for name in sorted(names):
+            utils.write_to_file("  * {}\n".format(name))
 
-    
-    # # entries > div:nth-child(157) > div.entryinfo > div.entryhead > b
-    # # entries > div:nth-child(157) > div.entryinfo > div:nth-child(2) > b
-    # # entries > div:nth-child(157) > div.entryinfo > div:nth-child(2) > a
-
-    # list_eventdata = page.query_selector_all(locator_entryitem_group)
-    
-    # list_events_unsorted = []
-
-    # for eventdata in list_eventdata:
-    #     text = eventdata.text_content().strip()
-    #     list_events_unsorted.append(text)
-
-    # return sorted(list(set(list_events_unsorted)))
-
-def get_quests(page):
-    page.click('#QuestLineData-module')
+def get_entry_items(page, module_locator):
+    page.click(module_locator)
     assert page.wait_for_selector("#entries > div:nth-child(1)")
 
-    locator_entryhead = constants.LOCATOR_ENTRYHEAD
-    locator_entryitem_group = constants.LOCATOR_ENTRYITEM.format("QuestIds")
+    list_entries_elements = page.query_selector_all('.entry')
+    dict_entries = {}
 
-    # event_elements_list = page.query_selector_all(selector_event_group)
+    for n in range(len(list_entries_elements)-1):
+        dict_entryitems = {}
+        
+        # entryhead_locator = f'.entry[{n+1}] .entryhead b'
+        entryhead_locator = f"//div[@class='entry'][{n+1}]//div[@class='entryhead']/b"
+        entryhead_element = page.query_selector(entryhead_locator)
+        entryhead = entryhead_element.inner_text().strip() if entryhead_element else "Unknown"
+
+        # entryitems_locator = f'.entry[{n+1}] .entryitem'
+        entryitems_locator = f"//div[@class='entry'][{n+1}]//div[@class='entryitem']"
+        list_entryitem_elements = page.query_selector_all(entryitems_locator)
+
+        for m in range(len(list_entryitem_elements)-1):
+            # entryitem_name_locator = f'.entry[{n+1}] .entryitem[{m+1}] b.entryitemname'
+            entryitem_name_locator = f"//div[@class='entry'][{n+1}]//div[@class='entryitem'][{m+1}]//b[@class='entryitemname']"
+            entryitem_name_element = page.query_selector(entryitem_name_locator)
+            entryitem_name = entryitem_name_element.inner_text().strip() if entryitem_name_element else "Unknown"
+
+            # entryitem_value_locator = f'.entry[{n+1}] .entryitem[{m+1}] a.entryitemvalue'
+            entryitem_value_locator = f"//div[@class='entry'][{n+1}]//div[@class='entryitem'][{m+1}]//a[@class='entryitemvalue']"
+            entryitem_value_element = page.query_selector(entryitem_value_locator)
+            entryitem_value = entryitem_value_element.inner_text().strip() if entryitem_value_element else "Unknown"
+
+            dict_entryitems[entryitem_name] = entryitem_value
+
+        dict_entries[entryhead] = dict_entryitems
     
-    # unsorted_event_list = []
-
-    # for event_element in event_elements_list:
-    #     text = event_element.text_content().strip()
-    #     unsorted_event_list.append(text)
-
-    # sorted_eventList = sorted(list(set(unsorted_event_list)))
-
-    # return sorted_eventList
+    return dict_entries
 
 
 def wait_a_bit(page):
